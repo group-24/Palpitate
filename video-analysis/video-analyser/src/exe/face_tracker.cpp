0ea13cc48ee8eb4af0601c8d2472681f972ab87c
Sam Coope <sam.j.coope@gmail.com>
2015-11-02 22:41:44 +0000

    program now analyses faces (although a bit shakily)

--- video-analysis/video-analyser/src/exe/face_tracker.cpp
+++ video-analysis/video-analyser/src/exe/face_tracker.cpp
@@ -3,38 +3,38 @@
 //
 // This file is part of FaceTracker.
 //
-// Redistribution and use in source and binary forms, with or without 
+// Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 //
 //     * The software is provided under the terms of this licence stricly for
 //       academic, non-commercial, not-for-profit purposes.
-//     * Redistributions of source code must retain the above copyright notice, 
+//     * Redistributions of source code must retain the above copyright notice,
 //       this list of conditions (licence) and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright 
-//       notice, this list of conditions (licence) and the following disclaimer 
-//       in the documentation and/or other materials provided with the 
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions (licence) and the following disclaimer
+//       in the documentation and/or other materials provided with the
 //       distribution.
-//     * The name of the author may not be used to endorse or promote products 
+//     * The name of the author may not be used to endorse or promote products
 //       derived from this software without specific prior written permission.
-//     * As this software depends on other libraries, the user must adhere to 
+//     * As this software depends on other libraries, the user must adhere to
 //       and keep in place any licencing terms of those libraries.
 //     * Any publications arising from the use of this software, including but
 //       not limited to academic journal and conference publications, technical
 //       reports and manuals, must cite the following work:
 //
-//       J. M. Saragih, S. Lucey, and J. F. Cohn. Face Alignment through 
-//       Subspace Constrained Mean-Shifts. International Conference of Computer 
+//       J. M. Saragih, S. Lucey, and J. F. Cohn. Face Alignment through
+//       Subspace Constrained Mean-Shifts. International Conference of Computer
 //       Vision (ICCV), September, 2009.
 //
-// THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED 
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO 
-// EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+// EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 // (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 // LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 // THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ///////////////////////////////////////////////////////////////////////////////
 #include <FaceTracker/Tracker.h>
@@ -79,7 +79,7 @@ void Draw(cv::Mat &image,cv::Mat &shape,cv::Mat &con,cv::Mat &tri,cv::Mat &visi)
     cv::line(image,p1,p2,c,1);
   }
   //draw points
-  for(i = 0; i < n; i++){    
+  for(i = 0; i < n; i++){
     if(visi.at<int>(i,0) == 0)continue;
     p1 = cv::Point(shape.at<double>(i,0),shape.at<double>(i+n,0));
     c = CV_RGB(255,0,0); cv::circle(image,p1,2,c);
@@ -96,7 +96,7 @@ int parse_cmd(int argc, const char** argv,
        (std::strcmp(argv[i],"--help") == 0)){
       std::cout << "track_face:- Written by Jason Saragih 2010" << std::endl
 	   << "Performs automatic face tracking" << std::endl << std::endl
-	   << "#" << std::endl 
+	   << "#" << std::endl
 	   << "# usage: ./face_tracker [options]" << std::endl
 	   << "#" << std::endl << std::endl
 	   << "Arguments:" << std::endl
@@ -167,13 +167,13 @@ int main(int argc, const char** argv)
   //set other tracking parameters
   std::vector<int> wSize1(1); wSize1[0] = 7;
   std::vector<int> wSize2(3); wSize2[0] = 11; wSize2[1] = 9; wSize2[2] = 7;
-  int nIter = 5; double clamp=3,fTol=0.01; 
+  int nIter = 5; double clamp=3,fTol=0.01;
   FACETRACKER::Tracker model(ftFile);
   cv::Mat tri=FACETRACKER::IO::LoadTri(triFile);
   cv::Mat con=FACETRACKER::IO::LoadCon(conFile);
-  
+
   //initialize camera and display window
-  cv::Mat frame,gray,im; double fps=0; char sss[256]; std::string text; 
+  cv::Mat frame,gray,im; double fps=0; char sss[256]; std::string text;
   CvCapture* camera = cvCreateCameraCapture(CV_CAP_ANY); if(!camera)return -1;
   int64 t1,t0 = cvGetTickCount(); int fnum=0;
   cvNamedWindow("Face Tracker",1);
@@ -183,26 +183,26 @@ int main(int argc, const char** argv)
 
   //loop until quit (i.e user presses ESC)
   bool failed = true;
-  while(1){ 
+  while(1){
     //grab image, resize and flip
     IplImage* I = cvQueryFrame(camera); if(!I)continue; frame = I;
-    if(scale == 1)im = frame; 
+    if(scale == 1)im = frame;
     else cv::resize(frame,im,cv::Size(scale*frame.cols,scale*frame.rows));
     cv::flip(im,im,1); cv::cvtColor(im,gray,CV_BGR2GRAY);
 
     //track this image
-    std::vector<int> wSize; if(failed)wSize = wSize2; else wSize = wSize1; 
+    std::vector<int> wSize; if(failed)wSize = wSize2; else wSize = wSize1;
     if(model.Track(gray,wSize,fpd,nIter,clamp,fTol,fcheck) == 0){
       int idx = model._clm.GetViewIdx(); failed = false;
-      Draw(im,model._shape,con,tri,model._clm._visi[idx]); 
+      Draw(im,model._shape,con,tri,model._clm._visi[idx]);
     }else{
       if(show){cv::Mat R(im,cvRect(0,0,150,50)); R = cv::Scalar(0,0,255);}
       model.FrameReset(); failed = true;
-    }     
-    //draw framerate on display image 
-    if(fnum >= 9){      
+    }
+    //draw framerate on display image
+    if(fnum >= 9){
       t1 = cvGetTickCount();
-      fps = 10.0/((double(t1-t0)/cvGetTickFrequency())/1e+6); 
+      fps = 10.0/((double(t1-t0)/cvGetTickFrequency())/1e+6);
       t0 = t1; fnum = 0;
     }else fnum += 1;
     if(show){
@@ -211,7 +211,7 @@ int main(int argc, const char** argv)
 		  CV_FONT_HERSHEY_SIMPLEX,0.5,CV_RGB(255,255,255));
     }
     //show image and check for user input
-    imshow("Face Tracker",im); 
+    imshow("Face Tracker",im);
     int c = cvWaitKey(10);
     if(c == 27)break; else if(char(c) == 'd')model.FrameReset();
   }return 0;
