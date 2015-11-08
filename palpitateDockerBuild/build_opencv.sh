4fae46b1ca8b33e2d09e439fa46a32bf304050e4
Michael Cypher <mikeecb@gmail.com>
2015-11-08 20:39:17 +0000

    Streaming video with heartrate superimposed not done. Audio does not work at the moment. Also it only works for 1 video (hardcoded). Also set up docker so I can run this on my mac without a 'VM'.

---
+++ palpitateDockerBuild/build_opencv.sh
@@ -0,0 +1,12 @@
+DIRECTORY=/home/
+
+git clone https://github.com/Itseez/opencv.git
+
+mkdir /opencv/release
+cd /opencv/release
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_PYTHON_SUPPORT=ON -D WITH_XINE=ON -D WITH_TBB=ON ..
+make && make install
+cd /
+
+# Leave OpenCV folder intact so we can get cascades for face detection
+# rm -rf opencv
