b7c0c27b4ae540c6729c5b6d41ee2dfa2d4bf9c3
coopie <sam.j.coope@gmail.com>
2015-11-25 22:16:04 +0000

    refactor worked

--- video_analysis/face_analyser.py
+++ video_analysis/face_analyser.py
@@ -4,110 +4,113 @@
 import numpy as np
 import cv2
 from scipy import signal
+from frame_inspector import FrameInspector
+import matplotlib.pyplot as plt
 
 PATH_TO_OPENCV_CASCADES = "C:\\Users\\Sam Coope\\Documents\\Programming\\opencv\\sources\\data\\haarcascades\\"
 SUBJECT_VIDEO_PATH = "D:\\HeartAV\\SensorData\\HeartAV_VideoFiles\\"
 
-import matplotlib.pyplot as plt
-l = []
-i = 0
+FRAME_RATE = 30
+WINDOW_SIZE = 4
 
 face_cascade = cv2.CascadeClassifier(PATH_TO_OPENCV_CASCADES + 'haarcascade_frontalface_alt2.xml')
 eye_cascade = cv2.CascadeClassifier(PATH_TO_OPENCV_CASCADES + 'haarcascade_eye.xml')
 
 video_capture = cv2.VideoCapture(SUBJECT_VIDEO_PATH + 'vp_035_01_00[2014][03][26][17][34]28.avi')
 
-# Moving averages for face position, starting values are typical in vidoes of subjects
-(steadyX, steadyY) = (400, 185)
-(steadyHeight, steadyWidth) = (250, 250)
-alpha_movement = 0.9
-alpha_face_size = 0.999
+inspector = FrameInspector(None)
 
-while i < 320:
-    i += 1
-# while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-    if not ret or frame is None:
-        print('no more frames from the video')
-        break
+def analyse_segment():
+    """some comment about what this does"""
 
-    # print(str(frame.shape))
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Moving averages for face position, starting values are typical in vidoes of subjects
+    (steadyX, steadyY) = (400, 185)
+    (steadyHeight, steadyWidth) = (250, 250)
+    alpha_movement = 0.9
+    alpha_face_size = 0.999
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for i in range(FRAME_RATE*WINDOW_SIZE + 100):
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+        if not ret or frame is None:
+            raise Error('video finished before analysing was complete')
+            break
 
-    if len(faces) > 1:
-        print('more than one face detected')
-    elif len(faces) == 0:
-        print('no faces detected')
-    else:
-        (x, y, w, h) = faces[0]
-        (steadyX, steadyY) = (
-            int(alpha_movement*steadyX + (1-alpha_movement)*x),
-            int(alpha_movement*steadyY + (1-alpha_movement)*y)
-        )
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        (steadyWidth, steadyHeight) = (
-            int(alpha_face_size*steadyWidth + (1-alpha_face_size)*w),
-            int(alpha_face_size*steadyHeight + (1-alpha_face_size)*h)
-        )
-        # draw a rectangle
-        cv2.rectangle(frame, (steadyX, steadyY), (steadyX+steadyWidth, steadyY+steadyHeight), (0, 255, 0), 2)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-        # params for setecting the face
+        if len(faces) > 1:
+            print('more than one face detected')
+        elif len(faces) == 0:
+            print('no faces detected')
+        else:
+            (x, y, w, h) = faces[0]
+            (steadyX, steadyY) = (
+                int(alpha_movement*steadyX + (1-alpha_movement)*x),
+                int(alpha_movement*steadyY + (1-alpha_movement)*y)
+            )
 
-        half_width = (steadyWidth // 2)
-        half_height = (steadyHeight // 2)
+            (steadyWidth, steadyHeight) = (
+                int(alpha_face_size*steadyWidth + (1-alpha_face_size)*w),
+                int(alpha_face_size*steadyHeight + (1-alpha_face_size)*h)
+            )
+            # draw a rectangle
+            cv2.rectangle(frame, (steadyX, steadyY), (steadyX+steadyWidth, steadyY+steadyHeight), (0, 255, 0), 2)
 
-        center_x = steadyX + half_width
-        center_y = steadyY + half_height
+            # params for setecting the face
 
-        from_middleX = 0.6
-        from_middleY = 0.7
+            half_width = (steadyWidth // 2)
+            half_height = (steadyHeight // 2)
 
-        top_right = (
-            steadyX + int((1-from_middleX) * half_width),
-            center_y
-        )
+            center_x = steadyX + half_width
+            center_y = steadyY + half_height
 
-        bottom_left = (
-            center_x + int(half_width * from_middleX),
-            center_y + int(half_height * from_middleY)
-        )
+            from_middleX = 0.6
+            from_middleY = 0.7
 
-        cv2.rectangle(frame, top_right, bottom_left, (255, 255, 0), 2)
+            top_right = (
+                steadyX + int((1-from_middleX) * half_width),
+                center_y
+            )
 
-        # just get the green color
-        interesting_pixels = frame[top_right[1]:bottom_left[1], top_right[0]:bottom_left[0], 1]
+            bottom_left = (
+                center_x + int(half_width * from_middleX),
+                center_y + int(half_height * from_middleY)
+            )
 
-        if i > 200:
-            l.append(interesting_pixels.mean())
+            # draw a rectangle
+            cv2.rectangle(frame, (steadyX, steadyY), (steadyX+steadyWidth, steadyY+steadyHeight), (0, 255, 0), 2)
+            cv2.rectangle(frame, top_right, bottom_left, (255, 255, 0), 2)
 
-        # cv2.imshow('sdas', interesting_pixels)
+            # just get the green color
+            interesting_pixels = frame[top_right[1]:bottom_left[1], top_right[0]:bottom_left[0]]
+            inspector.extract(frame)
+
+            # cv2.imshow('sdas', interesting_pixels)
 
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
 
-mean = sum(l)/len(l)
+    # g = plt.plot(range(len(l)), l)
+    # plt.show(g)
 
-print mean
+    # f, t, spectrogram = signal.spectrogram(l, 1.0, nperseg=30)
+    # plt.pcolormesh(t, f, spectrogram)
+    # plt.ylabel('Frequency [Hz]')
+    # plt.xlabel('Time [sec]')
+    # plt.show()
+    spectrograms = inspector.get_data()
+    inspector.done()
+    return spectrograms
 
-l = map(lambda x: x - mean, l)
 
-# g = plt.plot(range(len(l)), l)
-# plt.show(g)
-
-f, t, spectrogram = signal.spectrogram(l, 1.0, nperseg=30)
-plt.pcolormesh(t, f, spectrogram)
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
-plt.show()
+sps = analyse_segment()
--- video_analysis/frame_inspector.py
+++ video_analysis/frame_inspector.py
@@ -1,50 +1,57 @@
+import numpy as np
+from scipy import signal
+
 TIME_SECOND_WINDOW = 4
+FRAME_RATE = 30
 
 
-class frame_inspector(object):
+class FrameInspector(object):
     """This class inspects the frame of a video, then produces spectrograms of the
     desired features of the frame"""
 
-    def __init__(self, heartrates, frame_rate):
+    def __init__(self, heartrates):
         self.heartrates = heartrates
-        self.frame_rate = frame_rate
         self.frames_processed = 0
+        self.data = None
+        self.window = []
 
-    def extract(self, img, (x, y, width, height)):
-        half_width = (steadyWidth // 2)
-        half_height = (steadyHeight // 2)
+    def extract(self, frame):
+        """frame is the sliced pixel of the face"""
+        self.frames_processed += 1
+        # get the greenpixels
+        self.window.append(frame[:, :, 1].mean())
 
-        center_x = steadyX + half_width
-        center_y = steadyY + half_height
-
-        # the amounts from the middle of the face that we extend the square to
-        # get the interesting_pixels
-        from_middleX = 0.6
-        from_middleY = 0.8
-
-        # top right and bottom left of the square
-        top_right = (
-            steadyX + int((1-from_middleX) * half_width),
-            center_y
-        )
-
-        bottom_left = (
-            center_x + int(half_width * from_middleX),
-            center_y + int(half_height * from_middleY)
-        )
-
-        # just get the green color
-        interesting_pixels = frame[top_right[1]:bottom_left[1], top_right[0]:bottom_left[0], 1]
-        # self.pixels = 
-
-        if self.frames_processed = (self.frame_rate * TIME_SECOND_WINDOW):
-            # make the spectrograms
+        if self.frames_processed == (FRAME_RATE * TIME_SECOND_WINDOW):
+            print 'asdasdasdasdasdasdasd'
+            self.process_data()
         else:
-            self.frames_processed += 1
-
-
+            print self.frames_processed
+        # return the location of the smoothed face in the video
 
     def lost_frame(self):
-        throw Error("NOT DONE YET")
+        raise Error("NOT DONE YET")
 
-    def
+    def done(self):
+        """Called when processing of a video is finished, flushes the data"""
+        self.frames_processed = 0
+        self.data = None
+
+    def get_data(self):
+        return self.data
+
+    def process_data(self):
+        """makes the spectrigrams and adds it to data"""
+        self.frames_processed = 0
+        window = self.window
+        self.window = []
+
+        # normalise the time series
+        total = 0
+        mean = reduce(lambda acc, x: x + acc, window)/len(window)
+        window = map(lambda x: x - mean, window)
+
+        f, t, spectrogram = signal.spectrogram(window, 1.0, nperseg=30)
+        if self.data is None:
+            self.data = spectrogram
+        else:
+            self.data = np.concatenate(self.data, spectrogram)
